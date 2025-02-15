8e5cd3db242fbd67fe666435d1b6eb32bc57a645
xm <1062829664@qq.com>
2025-02-15 13:10:00 +0800

    feat(config): 添加 OpenAPI 配置和用户相关工具类
    - 新增 OpenApiConfig 类，配置 Swagger OpenAPI 文档信息
    - 添加 UserModifyRequest 类，用于用户信息修改请求
    - 移动 UserUtils 工具类，提供用户脱敏功能

---
+++ src/main/java/com/xm/xmgame/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.xm.xmgame.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("XM Game API")
+                        .description("游戏商城后端接口文档")
+                        .version("v1.0")
+                        .contact(new Contact()
+                                .name("XM")
+                                .email("1062829664@qq.com")
+                                .url("https://github.com/X1aoM1ngTX"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+} 
